37b3af5517807617a6e8ffe0bc3ba27e05a786aa
Yusuf Karim <ykarim250@gmail.com>
2017-07-18 21:57:28 -0400

    Moved loadXml() to FileLoad

--- WeatherWatch/src/main/java/prop/WeatherImgImport.java
+++ WeatherWatch/src/main/java/prop/WeatherImgImport.java
@@ -1,58 +1,37 @@
 package prop;
 
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 import util.FileLoad;
 import weather.Weather;
 
 import javax.imageio.ImageIO;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.awt.*;
 import java.io.IOException;
 
 public class WeatherImgImport {
 
-    private static final String ignoreDTDTag = "http://apache.org/xml/features/nonvalidating/load-external-dtd";
     private static String backgroundImageXmlName = "imageLocations.xml";
 
     private static FileLoad fileLoader = new FileLoad();
-    private static Element doc;
+    private static Element xml;
 
     private static boolean backgroundXmlLoaded = false;
 
     public static boolean loadBackgroundImageXml() {
         if (!backgroundXmlLoaded) {
-            Document dom;
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            try {
-                dbf.setFeature(ignoreDTDTag, false);
-                DocumentBuilder db = dbf.newDocumentBuilder();
-                dom = db.parse(fileLoader.load(backgroundImageXmlName));
-
-                doc = dom.getDocumentElement();
+            if (fileLoader.loadXml(backgroundImageXmlName) != null) {
+                xml = fileLoader.loadXml(backgroundImageXmlName);
                 backgroundXmlLoaded = true;
                 return true;
-            } catch (ParserConfigurationException pce) {
-                pce.printStackTrace();
-                return false;
-            } catch (SAXException sax) {
-                sax.printStackTrace();
-                return false;
-            } catch (IOException io) {
-                io.printStackTrace();
-                return false;
             }
         }
-        return true;
+        return false;
     }
 
     public static Image getBackgroundImage(Weather.WeatherCondition condition) {
         if (backgroundXmlLoaded) {
-            String imgLocation = getTextValue(doc, condition);
+            String imgLocation = getTextValue(xml, condition);
             if (imgLocation != null) {
                 if (!imgLocation.isEmpty()) {
                     try {
@@ -66,8 +45,8 @@ public class WeatherImgImport {
         return null;
     }
 
-    private static String getTextValue(Element doc, Weather.WeatherCondition tag) {
-        NodeList nl = doc.getElementsByTagName(tag.getName());
+    private static String getTextValue(Element xml, Weather.WeatherCondition tag) {
+        NodeList nl = xml.getElementsByTagName(tag.getName());
         if (nl.getLength() > 0 && nl.item(0).hasChildNodes()) {
             return nl.item(0).getFirstChild().getNodeValue();
         }
--- WeatherWatch/src/main/java/util/FileLoad.java
+++ WeatherWatch/src/main/java/util/FileLoad.java
@@ -1,10 +1,26 @@
 package util;
 
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
 
 public class FileLoad {
 
+    private static final String ignoreDTDTag = "http://apache.org/xml/features/nonvalidating/load-external-dtd";
+
     public InputStream load(String fileName) {
         return (FileLoad.class.getClassLoader().getResourceAsStream(fileName));
+    }
+
+    public Element loadXml(String fileName) {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            dbf.setFeature(ignoreDTDTag, false);
+            return dbf.newDocumentBuilder().parse(load(fileName)).getDocumentElement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
