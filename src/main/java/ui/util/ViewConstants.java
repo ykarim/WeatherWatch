7ffe415bf0736c4b1dba08da789b71e78f5f933d
Yusuf Karim <ykarim250@gmail.com>
2018-08-25 11:20:29 -0400

    Fixed incorrect initial dimensions and icon storage
    Removed old gui folder from resources

--- src/main/java/ui/util/ViewConstants.java
+++ src/main/java/ui/util/ViewConstants.java
@@ -3,10 +3,10 @@ package ui.util;
 public class ViewConstants {
 
     public static final String PROGRAM_TITLE = "WeatherWatch";
-    public static final String PROGRAM_ICON_LOCATION = "gui/WeatherWatchIcon.png";
+    public static final String PROGRAM_ICON_LOCATION = "ui/icons/WeatherWatchIcon.png";
 
-    public static int PROGRAM_WIDTH = 500;
-    public static int PROGRAM_HEIGHT = 300;
+    public static int PROGRAM_WIDTH = 300;
+    public static int PROGRAM_HEIGHT = 500;
 
     public static int NOTIFICATION_WIDTH = 250;
     public static int NOTIFICATION_HEIGHT = 75;
