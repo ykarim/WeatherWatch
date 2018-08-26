45f397159948e745a1f881a71a5bcf88a6e52422
Yusuf Karim <ykarim250@gmail.com>
2018-08-25 23:04:54 -0400

    Formatted dashboard fonts and implemented basic weather background mechanism
    * Added DateTime format for last updated time

--- src/main/java/ui/dashboard/DashboardController.java
+++ src/main/java/ui/dashboard/DashboardController.java
@@ -5,14 +5,19 @@ import dao.WeatherDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import model.Weather;
 import net.APIAccess;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import ui.settings.SettingsScene;
 import ui.util.AppController;
 import ui.util.Bundle;
 import ui.util.SceneManager;
 import util.Constants;
+import util.FileImport;
+
+import java.math.BigDecimal;
 
 public class DashboardController implements AppController {
 
@@ -48,11 +53,15 @@ public class DashboardController implements AppController {
         if (weatherDAO.getLatestWeather() != null) {
             Weather currentWeather = weatherDAO.getLatestWeather();
 
+            vBox_weatherPane.setBackground(new Background(getBackgroundImage(currentWeather.getCondition())));
             lbl_currentTemp.setText(
-                    currentWeather.getTemperature().getTemperatureValue(Constants.PREFERRED_UNIT).toPlainString());
+                    currentWeather.getTemperature().getTemperatureValue(Constants.PREFERRED_UNIT).setScale(0).toPlainString());
             lbl_currentCond.setText(currentWeather.getCondition());
             lbl_location.setText(currentWeather.getLocation().toString());
-            lbl_timeText.setText("Last Updated: " + currentWeather.getWeatherTime().toString());
+
+            //Create date pattern and set time label to last received weather data time
+            DateTimeFormatter timeFormat = DateTimeFormat.forPattern("hh:mm a");
+            lbl_timeText.setText("Last Updated: " + currentWeather.getWeatherTime().toString(timeFormat));
         }
     }
 
@@ -62,12 +71,17 @@ public class DashboardController implements AppController {
     @Override
     public void refresh() {
         //Pull new weather data
-        Weather weather = APIAccess.requestWeatherConditions(Constants.PREFERRED_LOCATION);
+        APIAccess.requestWeatherConditions(Constants.PREFERRED_LOCATION);
+        Weather weather = weatherDAO.getLatestWeather();
 
-        lbl_currentTemp.setText(weather.getTemperature().getTemperatureValue(Constants.PREFERRED_UNIT).toPlainString());
+        vBox_weatherPane.setBackground(new Background(getBackgroundImage(weather.getCondition())));
+        lbl_currentTemp.setText(weather.getTemperature().getTemperatureValue(Constants.PREFERRED_UNIT).setScale(0, BigDecimal.ROUND_HALF_UP).toPlainString());
         lbl_currentCond.setText(weather.getCondition());
         lbl_location.setText(weather.getLocation().toString());
-        lbl_timeText.setText("Last Updated: " + weather.getWeatherTime().toString());
+
+        //Create date pattern and set time label to last received weather data time
+        DateTimeFormatter timeFormat = DateTimeFormat.forPattern("hh:mm a");
+        lbl_timeText.setText("Last Updated: " + weather.getWeatherTime().toString(timeFormat));
     }
 
     @FXML
@@ -78,5 +92,18 @@ public class DashboardController implements AppController {
     @FXML
     private void handleSettingsButton(ActionEvent event) {
         SceneManager.addScene(new SettingsScene());
+    }
+
+    /**
+     * Creates BackgroundImage object by retrieving condition and provider dependent image
+     * TODO: Create image manager to extract based upon provider and defaults instead of manual file input
+     *
+     * @param condition current weather condition used to determine background
+     */
+    private BackgroundImage getBackgroundImage(String condition) {
+        return new BackgroundImage(
+                FileImport.importImage(FileImport.importLocalFile("backgrounds/owm/" + condition.toLowerCase() + ".jpg")),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT);
     }
 }
